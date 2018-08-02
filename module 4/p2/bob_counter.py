e0a70ed0485f30def6d6737b267d919be52af2d3
akshitha111 <akshitha2408@msitprogram.net>
2018-08-02 17:41:22 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- module 4/p2/bob_counter.py
+++ module 4/p2/bob_counter.py
@@ -1,13 +1,13 @@
 '''Write a program that prints the number of times the string 'bob' occurs in s.'''
 
 def main():
-        '''Main function.'''
+    '''Main function'''
     str_1 = input()
     str_2 = 'bob'
     cnt = 0
     for i in range(len(str_1)-2):
         if str_1[i]+str_1[i+1]+str_1[i+2] == str_2:
-	cnt = cnt+1
+            cnt = cnt+1
     print(cnt)
 if __name__ == "__main__":
-	main()
+    main()
