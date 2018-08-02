d7c5d6afe7976a844edc1c2dd4783030b3803ee5
akshitha111 <akshitha2408@msitprogram.net>
2018-08-02 17:24:45 +0530

    " -> 0 of 5 passed. pylint: 0/0 "

--- module 4/p2/bob_counter.py
+++ module 4/p2/bob_counter.py
@@ -6,12 +6,12 @@ Number of times bob occurs is: 2'''
 
 def main () :
 	'''Main Function'''
-	str_1 = input ()
-	str_2 = 'bob'
-	cnt = 0
-    for i in range(len(str_1)-2):
-    if str_1[i]+str_1[i+1]+str_1[i+2] == str_2:
-    cnt = cnt+1
+    str_1 = input()
+    str_2 = 'bob'
+    cnt = 0
+    for i in range (len(str_1)-2):
+        if str_1[i]+str_1[i+1]+str_1[i+2] == str_2:
+        cnt = cnt+1
 	print(cnt)
 	if __name__ == "__main__":
         main()
