55075e54fb7a72978c5aa35dffe431919e3506f2
akshitha111 <akshitha2408@msitprogram.net>
2018-08-04 11:53:19 +0530

    " -> 0 of 7 passed. pylint: 10.00/10 "

--- module 6/p3/digit_product.py
+++ module 6/p3/digit_product.py
@@ -1,26 +1,18 @@
-'''
-Given a  number int_input, find the product of all the digits
-example: 
-	input: 123
-	output: 6
-'''
+""" digit product """
 def main():
     '''
     Read any number from the input, store it in variable int_input.
     '''
-    int_input = int(input())
-    count = 0
-    s = int(input())
-    di = 0
-    p = i
-    while n = 0:
-    	d = n%10
-    	n = n//10
-    	p = p*d
-    print(p)
-
-    	pass
-
+COUNT = 0
+i = 1
+S = int(input())
+DI = 0
+P = i
+while S == 0:
+    DI = S%10
+    S = S//10
+    P = P*DI
+print(P)
 
 if __name__ == "__main__":
     main()
