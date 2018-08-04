f01e40f3e506a6bc727fc19b549c1d9c2751d5d6
akshitha111 <akshitha2408@msitprogram.net>
2018-08-04 10:22:33 +0530

    " -> 2 of 7 passed. pylint: 9.17/10 "

--- module 6/p1/fizz_buzz.py
+++ module 6/p1/fizz_buzz.py
@@ -11,8 +11,8 @@ def main():
 N = int(input())
 for i in range(1, (N+1)):
     if i%3 == 0 & i%5 == 0:
-        print("Fizz")
-        print("Buzz")
+        print("FizzBuzz")
+        
     elif i%3 == 0:
         print("Fizz")
     elif i%5 == 0:
