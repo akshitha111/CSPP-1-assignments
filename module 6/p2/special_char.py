c443fd98c38917e42d033a82abfbabcf9a3eb46f
akshitha111 <akshitha2408@msitprogram.net>
2018-08-04 11:21:01 +0530

    " -> 1 of 7 passed. pylint: 4.00/10 "

--- module 6/p2/special_char.py
+++ module 6/p2/special_char.py
@@ -12,7 +12,9 @@ str_output = ""
 for char in str_input:
     if str_input == '!' or str_input == '@' or str_input == '#' or str_input == '$' or str_input == '%' or str_input == '^' or str_input == '&' or str_input == '*':
             str_output = str_output + ""
-            print(str_output)
+    else:
+        str_output = str_output + char
+    print(str_output)
 
 if __name__ == "__main__":
     main()
