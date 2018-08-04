6b4ed5c03503d07d3b3b7d23f64bef8d2f315dc6
akshitha111 <akshitha2408@msitprogram.net>
2018-08-04 12:15:08 +0530

    " -> 0 of 7 passed. pylint: 4.44/10 "

--- module 6/p3/digit_product.py
+++ module 6/p3/digit_product.py
@@ -3,17 +3,12 @@ def main():
     '''
     Read any number from the input, store it in variable int_input.
     '''
-COUNT = 0
-i = 1
 S = int(input())
-DI = 0
-P = i
-while S == 0:
-    DI = S%10
-    S = S//10
-    P = P*DI
-    i += 1
-print(P)
+for i in S:
+    A = S%10
+    S = S/10
+    A = A * i
+print(A)
 
 if __name__ == "__main__":
     main()
