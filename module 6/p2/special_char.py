22b2e413a08abefef0aae845b865d8a623e78432
akshitha111 <akshitha2408@msitprogram.net>
2018-08-04 10:47:56 +0530

    " -> 0 of 7 passed. pylint: 0/0 "

--- module 6/p2/special_char.py
+++ module 6/p2/special_char.py
@@ -10,8 +10,9 @@ def main():
     str_input = input()
     str_output = ""
     for char in str_input:
-    	if str_input = '!' or str_input = '@' or str_input = '#' or str_input = '$' or str_input = '%' or str_input = '^' or str_input = '&' or str_input = '*':
+    	if str_input == '!' or str_input == '@' or str_input == '#' or str_input == '$' or str_input == '%' or str_input == '^' or str_input == '&' or str_input == '*':
     		str_output = str_output + ""
+    		print(str_output)
 
     		if __name__ == "__main__":
     main()
