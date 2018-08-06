0c09af73e89ffcf60ee741f05b2c83277e63ec61
akshitha111 <akshitha2408@msitprogram.net>
2018-08-06 17:23:56 +0530

    odd

---
+++ module 7/Functions - Odd Exercise/odd.py
@@ -0,0 +1,11 @@
+""" find odd """
+def odd(x_1):
+    """ odd """
+    return bool(x_1%2 != 0)
+def main():
+    """ odd """
+    data = input()
+    print(odd(int(data)))
+
+if __name__ == "__main__":
+    main()
