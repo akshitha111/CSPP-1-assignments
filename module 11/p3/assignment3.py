c91ad110019b10f2a91c1f6105faa0e4908f960e
akshitha111 <akshitha2408@msitprogram.net>
2018-08-11 16:43:18 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

---
+++ module 11/p3/assignment3.py
@@ -0,0 +1,24 @@
+"""word"""
+def is_valid_word(word_1, hand_1, word_list):
+    """word"""
+    if word_1 not in word_list:
+        return False
+    for i_1 in word_1:
+        if i_1 not in hand_1.keys():
+            return False
+    return True
+def main():
+    """word"""
+    word_1 = input()
+    n_1 = int(input())
+    adict_1 = {}
+    for i in range(n_1):
+        data = input()
+        i = i+1
+        i = i-1
+        l_1 = data.split()
+        adict_1[l_1[0]] = int(l_1[1])
+    l_2 = input().split()
+    print(is_valid_word(word_1, adict_1, l_2))
+if __name__ == "__main__":
+    main()
