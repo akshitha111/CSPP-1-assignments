b5907a8ed6111dc4fb7fc7a6823a27d8b1effd28
akshitha111 <akshitha2408@msitprogram.net>
2018-08-11 16:18:15 +0530

    " -> 9 of 9 passed. pylint: 8.24/10 "

---
+++ module 11/p1/assignment1.py
@@ -0,0 +1,24 @@
+"""game"""
+def get_word_score(word_1, n_1):
+    """string"""
+    sum_1 = 0
+    dictionary_ = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
+    length_1 = len(word_1)
+    
+    for i in word_1:
+        if i in dictionary_.keys():
+            sum_1 = sum_1 + dictionary_[i]
+    sum_1 = sum_1*length_1
+    if n_1 == length_1:
+        sum_1 += 50
+    return sum_1
+    
+def main():
+    '''
+    Main function for the given problem
+    '''
+    data = input()
+    data = data.split(" ")
+    print(get_word_score(data[0], int(data[1])))
+if __name__ == "__main__":
+    main()
--- module 8/power using recursion/power_recr.py
+++ module 8/power using recursion/power_recr.py
@@ -1,21 +1,12 @@
-''' # Exercise: PowerRecr
-# This function takes in two numbers and returns one number.
-'''
-def recurPower(base, exp):
-    '''
-    base: int or float.
-    exp: int >= 0
- 
-    returns: int or float, base^exp
-    '''
-    if exp == 1:
-        return base
-    else:
-        return base * recurPower(base, exp-1)
-def main():
-    data = input()
-    data = data.split()
-    print(recurPower(float(data[0]), int(data[1])))
+def foo(x, a):
+   """
+   x: a positive integer argument
+   a: a positive integer argument
 
-if __name__ == "__main__":
-    main()
+   returns an integer
+   """
+   count = 0
+   while x >= a:
+      count += 1
+      x = x - a
+   return count
