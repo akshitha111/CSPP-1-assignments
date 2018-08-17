d6fc2a632098e8e979c07489c93c99f96c9fa175
akshitha111 <akshitha2408@msitprogram.net>
2018-08-17 17:55:15 +0530

    " -> 3 of 3 passed. pylint: 7.12/10 "

--- module 16/CodeCampDocumentDistance/document_distance.py
+++ module 16/CodeCampDocumentDistance/document_distance.py
@@ -12,18 +12,18 @@ def words_list(doc):
         words1 = []
     regex = re.compile("[^a-z]")
     for w in words:
-        words1.append(regex.sub("",w))
+        words1.append(regex.sub("", w))
     return words1
-def remove_Stop_words(words,StopWords):
+def remove_Stop_words(words, StopWords):
     words1 = []
     for w in words:
-        if w not in StopWords and len(w)>0:
+        if w not in StopWords and len(w)> 0:
             words1.append(w)
     return words1
-def createDictionary(dictionary,words,index):
+def createDictionary(dictionary, words, index):
     for w in words:
         if w not in dictionary.keys():
-            dictionary[w] = [0,0]
+            dictionary[w] = [0, 0]
         dictionary[w][index] += 1
     return dictionary
 def compute(dictionary):
@@ -41,11 +41,9 @@ def similarity(dict1, dict2):
     words_1 = remove_Stop_words(words_1, stopWords)
     words_2 = remove_Stop_words(words_2, stopWords)
     dictionary = dict()
-    dictionary = createDictionary(dictionary,words_1,0)
-    dictionary = createDictionary(dictionary,words_2,1)
-    return compute(dictionary)  
-
-
+    dictionary = createDictionary(dictionary, words_1, 0)
+    dictionary = createDictionary(dictionary, words_2, 1)
+    return compute(dictionary)
 def load_stopwords(filename):
     '''
         loads stop words from a file and returns a dictionary
