21cc3d48b5fee34bfeeb47ebe8263a33e8fcf6dc
akshitha111 <akshitha2408@msitprogram.net>
2018-08-17 10:20:26 +0530

    " -> 0 of 2 passed. pylint: 9.35/10 "

--- module 12/p2/social_network.py
+++ module 12/p2/social_network.py
@@ -51,7 +51,8 @@ def delete_person(network, arg1):
         update the network dictionary and return it
     '''
     # remove the pass below and start writing your code
-    del network[arg1]
+    if arg1 in network:
+        del network[arg1]
     return network
     
 
