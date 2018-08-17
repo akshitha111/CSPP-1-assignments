8a26978bed651c4abdceb371fbfd791ece26d01f
akshitha111 <akshitha2408@msitprogram.net>
2018-08-17 10:10:58 +0530

    " -> 0 of 2 passed. pylint: 9.03/10 "

--- module 12/p2/social_network.py
+++ module 12/p2/social_network.py
@@ -14,12 +14,11 @@ def follow(network, arg1, arg2):
         update the network dictionary and return it
     '''
     # remove the pass below and start writing your code
-    print(type(network))
+    network={}
     if arg1 in network:
         network[arg1].append(arg2)
     else:
         network[arg1] = []
-
         network[arg1].append(arg2)
     return network
 
