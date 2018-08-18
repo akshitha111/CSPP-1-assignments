361eff87b802e524a2c69098ab4c1240e7c3f3ec
akshitha111 <akshitha2408@msitprogram.net>
2018-08-18 13:00:11 +0530

    " -> 0 of 1 passed. pylint: 10.00/10 "

--- module 18/p2/search.py
+++ module 18/p2/search.py
@@ -43,9 +43,9 @@ def search(search_index, query):
         query_sent.append(list_1)
     for i in query_sent:
         a_empty = []
-    for j in i:
-        k = 0
-        b_empty = []
+        for j in i:
+            k = 0
+            b_empty = []
     for j in search_index:
         b_empty = search_index[j]
         while k < len(b_empty):
@@ -61,7 +61,7 @@ def process_queries(search_index, queries):
         iterate through all the queries and call the search function
         print the results returned by search function
     '''
-search(search_index, queries)
+    search(search_index, queries)
 
 def main():
     '''
