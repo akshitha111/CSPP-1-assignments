f22fe2d6eab0060bceafc35e29fbeaeed60f5d9c
akshitha111 <akshitha2408@msitprogram.net>
2018-08-23 17:23:02 +0530

    " -> 0 of 8 passed. pylint: 4.47/10 "

--- module 20/CodeCampMatrixOperations/matrix_operations.py
+++ module 20/CodeCampMatrixOperations/matrix_operations.py
@@ -53,14 +53,14 @@ def read_matrix():
         error message should be "Error: Invalid input for the matrix"
     '''
     rows,columns = [int(i) for i in input().split(",")]
-    matrix = []
+    matrix_1 = []
     for i in range(rows):
         lst = [int(i) for i in input().split(" ")]
         if len(lst) != columns:
             print("Error: Invalid input for the matrix")
             return None
-        matrix.append(lst)
-    return matrix
+        matrix_1.append(lst)
+    return matrix_1
 
 def main():
     # read matrix 1
