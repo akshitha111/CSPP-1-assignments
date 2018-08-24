91182a52b8e3f0909cc5bdaad2652d41ea98f94a
akshitha111 <akshitha2408@msitprogram.net>
2018-08-24 17:12:25 +0530

    " -> 0 of 14 passed. pylint: -6.62/10 "

---
+++ module 21/CodeCampTicTacToe/tic tac toe.py
@@ -0,0 +1,74 @@
+def checkInput(matrix):
+	for i in matrix:
+		for j in i:
+			if j not in "o x .":
+				return False
+	return True
+def CheckGame(matrix):
+	count_o = 0
+	count_x = 0
+	for row in matrix:
+		for i in row:
+			if i == 'o':
+				count_o += 1
+			elif i == 'x':
+				count_x += 1
+def rows(matrix):
+	winner_x = False
+	winner_o = False
+	if row.count("x") == 3:
+		winner_x = True
+	if row.count("o") == 3:
+		winner_o = True
+	if winner_o and winner_x:
+		print("invalid game")
+		exit()
+	if winner_x:
+		return (True,"x")
+	if winner_o:
+		return (True,"o")
+def column(matrix):
+	transpose = []
+	for i in range(len(matrix)):
+		row = []
+		for j in range(len(matrix[0])):
+			row.append(matrix[j][i])
+		transpose.append(row)
+	return rows(transpose)
+def diagonal(matrix):
+	d1 = []
+	for i in range(len(matrix)):
+		d1.append[matrix[i][i]]
+	if d1.count("o") == 3:
+		return (True,"o")
+	if d1.count("x") == 3:
+		return (True,"x")
+	d2 = []
+	j = len(matrix[0])-1
+	for i in range(len(matrix)):
+		d2.append(matrix[i][j])
+		j = j-1
+	if d2.count("o") == 3:
+		return
+	if d2.count("x") == 3:
+		return
+	return (False,0)
+
+
+
+
+
+def main():
+	matrix = []
+	i = 0
+	while i < 3:
+		lst = input().split(" ")
+		matrix.append(lst)
+		i = i + 1
+		print(matrix)
+		if checkGame(matrix):
+			checkWinner
+		else:
+			print("Invalid game")
+	if checkInput(matrix):
+		main()
--- module 4/p1/vowels_counter.py
+++ module 4/p1/vowels_counter.py
@@ -1,9 +1,7 @@
-def normalize(numbers):
-    max_number = max(numbers)
-    for i in range(len(numbers)):
-        numbers[i] /= float(max_number)
-    return numbers
-try:
-      normalize([0, 0, 0])
-except ZeroDivisionError:
-      print('Invalid maximum element')
+s = str(input())
+vowels = ['a','e','i','o','u']
+count = 0
+for i in vowels:
+	if i in s:
+		count = count+1
+print(count)
