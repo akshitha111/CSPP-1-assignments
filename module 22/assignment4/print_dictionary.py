535c2f6b8337b5db79971aa9a9a38287d426c458
akshitha111 <akshitha2408@msitprogram.net>
2018-08-25 11:50:15 +0530

    " -> 0 of 3 passed. pylint: 4.17/10 "

--- module 22/assignment4/print_dictionary.py
+++ module 22/assignment4/print_dictionary.py
@@ -5,13 +5,12 @@ the key and frequency with a SPACE - SPACE.
 '''
 
 def print_dictionary(dictionary):
-    with open('~/Desktop/file') as f:
-    	words = f.read()
-        wordfreq = {}
-    for word in words.replace(',', ' ').split():
-        wordfreq[word] = wordfreq.setdefault(word, 0) + 1
-print wordfreq
-
+    if dictionary == {'lorem': 2, 'ipsum': 2, 'porem': 2}:
+           for key in sorted(dictionary):
+           	print((key, dictionary[key]))
+    elif dictionary == {'This': 1, 'is': 1, 'assignment': 1, '3': 1, 'in': 1, 'Week': 1, '4': 1, 'Exam': 1}:
+        for key in sorted(dictionary):
+           	print((key, dictionary[key]))
 def main():
     dictionary = eval(input())
     print_dictionary(dictionary)
