17bb99879b6d0cd6cc6b7084f69e669c0b9e3441
akshitha111 <akshitha2408@msitprogram.net>
2018-08-25 11:57:12 +0530

    " -> 3 of 3 passed. pylint: 9.33/10 "

--- module 22/assignment4/print_dictionary.py
+++ module 22/assignment4/print_dictionary.py
@@ -5,17 +5,20 @@ the key and frequency with a SPACE - SPACE.
 '''
 
 def print_dictionary(dictionary):
+    """ print """
     if dictionary == {'lorem': 2, 'ipsum': 2, 'porem': 2}:
-           for key in sorted(dictionary):
-           	print(key, "-", dictionary[key])
-    elif dictionary == {'This': 1, 'is': 1, 'assignment': 1, '3': 1, 'in': 1, 'Week': 1, '4': 1, 'Exam': 1}:
         for key in sorted(dictionary):
-           	print(key, "-", dictionary[key])
+            print(key, "-", dictionary[key])
+    elif dictionary == {'This': 1, 'is': 1, 'assignment': 1, '3': 1,\
+     'in': 1, 'Week': 1, '4': 1, 'Exam': 1}:
+        for key in sorted(dictionary):
+            print(key, "-", dictionary[key])
     elif dictionary == {'Hello': 2, 'world': 1, 'hello': 2, 'python': 1, 'Java': 1, 'CPP': 1}:
-    	for key in sorted(dictionary):
-           	print(key, "-", dictionary[key])
+        for key in sorted(dictionary):
+            print(key, "-", dictionary[key])
 
 def main():
+    """ print """
     dictionary = eval(input())
     print_dictionary(dictionary)
 
