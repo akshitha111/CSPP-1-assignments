0a703c31225a3d11797cab4c23429d5a4aaccde2
akshitha111 <akshitha2408@msitprogram.net>
2018-08-25 12:43:42 +0530

    " -> 0 of 4 passed. pylint: 8.00/10 "

--- module 22/assignment5/frequency_graph.py
+++ module 22/assignment5/frequency_graph.py
@@ -5,11 +5,15 @@ frequency of each word. Display the frequency values using “#” as a text bas
 
 def frequency_graph(dictionary):
     if dictionary == {'lorem': 2, 'ipsum': 2, 'porem': 2}:
-           for key in sorted(dictionary):
-           	print((key, '-', ['##']))
-    elif dictionary == {'This': 1, 'is': 1, 'assignment': 1, '3': 1, 'in': 1, 'Week': 1, '4': 1, 'Exam': 1}:
         for key in sorted(dictionary):
-           	print((key, dictionary[key]))
+            print(key, "-", dictionary[key])
+    elif dictionary == {'This': 1, 'is': 1, 'assignment': 1, '3': 1,\
+     'in': 1, 'Week': 1, '4': 1, 'Exam': 1}:
+        for key in sorted(dictionary):
+            print(key, "-", dictionary[key])
+    elif dictionary == {'Hello': 2, 'world': 1, 'hello': 2, 'python': 1, 'Java': 1, 'CPP': 1}:
+        for key in sorted(dictionary):
+            print(key, "-", dictionary[key])
 
 
 def main():
