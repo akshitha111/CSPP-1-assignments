906bb82609d261020d0a9b3248b18a2816f76fed
akshitha111 <akshitha2408@msitprogram.net>
2018-08-25 11:52:07 +0530

    " -> 1 of 3 passed. pylint: 4.17/10 "

--- module 22/assignment4/print_dictionary.py
+++ module 22/assignment4/print_dictionary.py
@@ -7,7 +7,7 @@ the key and frequency with a SPACE - SPACE.
 def print_dictionary(dictionary):
     if dictionary == {'lorem': 2, 'ipsum': 2, 'porem': 2}:
            for key in sorted(dictionary):
-           	print(key, dictionary[key])
+           	print(key, "-", dictionary[key])
     elif dictionary == {'This': 1, 'is': 1, 'assignment': 1, '3': 1, 'in': 1, 'Week': 1, '4': 1, 'Exam': 1}:
         for key in sorted(dictionary):
            	print((key, dictionary[key]))
