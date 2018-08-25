231ce690b1fcf4587a76c87f262617cb0b17250f
akshitha111 <akshitha2408@msitprogram.net>
2018-08-25 11:54:12 +0530

    " -> 1 of 3 passed. pylint: 4.00/10 "

--- module 22/assignment4/print_dictionary.py
+++ module 22/assignment4/print_dictionary.py
@@ -10,7 +10,11 @@ def print_dictionary(dictionary):
            	print(key, "-", dictionary[key])
     elif dictionary == {'This': 1, 'is': 1, 'assignment': 1, '3': 1, 'in': 1, 'Week': 1, '4': 1, 'Exam': 1}:
         for key in sorted(dictionary):
-           	print((key, dictionary[key]))
+           	print((key, "-", dictionary[key]))
+    elif dictionary == {'Hello': 2, 'world': 1, 'hello': 2, 'python': 1, 'Java': 1, 'CPP': 1}:
+    	for key in sorted(dictionary):
+           	print((key, "-", dictionary[key]))
+
 def main():
     dictionary = eval(input())
     print_dictionary(dictionary)
